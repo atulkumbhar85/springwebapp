f579c8f4851cdc8b2389cfe5a3b96a392425fd87
student <atul.kumbhar1985@gmail.com>
2021-05-29 14:59:09 +0000

    spring app commit 1

---
+++ src/test/java/simplilearn/com/springapp/SpringappApplicationTests.java
@@ -0,0 +1,13 @@
+package simplilearn.com.springapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
